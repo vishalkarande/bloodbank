e982d89df36d920731509411e8336bd2993eb2b3
vishalkarande <vishalkarande6@gmail.com>
2020-07-21 14:44:53 +0530

    firebase data Display

--- app/src/main/java/com/example/blood_bank/register.java
+++ app/src/main/java/com/example/blood_bank/register.java
@@ -1,16 +1,21 @@
 package com.example.blood_bank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class register extends AppCompatActivity {
 
@@ -22,6 +27,8 @@ public class register extends AppCompatActivity {
    // FirebaseAuth auth;
     DatabaseReference reff;
     FirebaseDatabase firebaseDatabase;
+    TextView display;
+    Button displaybutton;
 
 
 
@@ -35,13 +42,15 @@ public class register extends AppCompatActivity {
         age= (EditText) findViewById(R.id.age);
         blood= (EditText) findViewById(R.id.blood);
         add= (Button) findViewById(R.id.add);
-
+        display= (TextView) findViewById(R.id.namedisplay);
+        displaybutton=(Button)findViewById(R.id.display);
+        firebaseDatabase= FirebaseDatabase.getInstance();
     //    reff= FirebaseDatabase.getInstance().getReference().child()
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firebaseDatabase= FirebaseDatabase.getInstance();
+
                 reff = firebaseDatabase.getReference("donate").child("user");
 
 
@@ -62,7 +71,32 @@ public class register extends AppCompatActivity {
         });
 
 
+        //featching data
 
+        displaybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                reff = firebaseDatabase.getReference("donate").child("user").child("joker");
+
+               reff.addValueEventListener(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                       String username = snapshot.child("bloodgroup").getValue().toString();
+
+                       Toast.makeText(register.this, "value "+username, Toast.LENGTH_LONG).show();
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError error) {
+
+                       Toast.makeText(register.this, "Error", Toast.LENGTH_LONG).show();
+                   }
+               });
+
+            }
+        });
 
 
     }
